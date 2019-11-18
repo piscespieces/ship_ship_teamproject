dfb81d7376c404789bd16069178cd1c7801930ee
Eric <Aerelioyanadel@gmail.com>
2019-11-18 08:02:59 -0500

    set root of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  # get 'hello_world', to: 'hello_world#index'
+root "dashboard#show"
+resources :shipments, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
