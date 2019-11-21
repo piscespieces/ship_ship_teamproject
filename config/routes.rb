76b82c18d68230b6eb75ec02659056a200028f04
Andy Weiss <andyweiss1982@gmail.com>
2019-11-21 15:22:49 -0500

    mess with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "landing#show"
-  resources :dashboard
+  resource :dashboard, only: [:show]
   resources :shipments, only: [:create, :show, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
