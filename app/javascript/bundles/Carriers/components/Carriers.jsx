7a164ac6bf874cebb92454d2c95b403bf8256ba2
Andres Urdaneta <aeum3893@gmail.com>
2019-11-22 17:29:59 -0500

    get the carriers array filtered by uniqeness

--- app/javascript/bundles/Carriers/components/Carriers.jsx
+++ app/javascript/bundles/Carriers/components/Carriers.jsx
@@ -2,18 +2,33 @@ import React from 'react'
 
 export default class Carriers extends React.Component {
     state = {
+        carriers: this.props.shipment.rates,
+        provider: []
+    }
+
+    componentDidMount() {
+        this.getCarriers()
+    }
+
+    getCarriers = () => {
+        const uniqCarriers = this.state.carriers.map(c => { return c.carrier })
+        const filterCarriers = uniqCarriers.filter(function (car, index) {
+            return uniqCarriers.indexOf(car) === index
+        })
+        this.setState({ provider: filterCarriers })
     }
 
     handleSelectChange = (e) => {
     }
 
     render() {
+        const { carriers, provider } = this.state
         return (
             <>
                 <h1>Carriers JSX</h1>
                 <div>
                     {
-                        console.log(this.props.shipment.rates.map(d => { return d.carrier }))
+                        console.log(provider)
                     }
                 </div>
                 {/* <h1>Carriers JSX</h1>
