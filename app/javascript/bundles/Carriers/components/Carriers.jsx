794a4786fd410541e829378449a17bd6816ef408
Andres Urdaneta <aeum3893@gmail.com>
2019-11-22 10:04:09 -0500

    targeting certain parts of the api

--- app/javascript/bundles/Carriers/components/Carriers.jsx
+++ app/javascript/bundles/Carriers/components/Carriers.jsx
@@ -4,9 +4,11 @@ export default class Carriers extends React.Component {
     state = {}
 
     render() {
+        console.log(this.props.shipment)
         return (
             <>
                 <h1>Carriers JSX</h1>
+                {this.props.shipment.buyer_address.street1}
             </>
         )
     }
