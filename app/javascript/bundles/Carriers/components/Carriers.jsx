692410fb4d9c03f8300fd2885b986051a9570672
Andres Urdaneta <aeum3893@gmail.com>
2019-11-21 19:26:42 -0500

    created carriers jsx component

---
+++ app/javascript/bundles/Carriers/components/Carriers.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Carriers extends React.Component {
+    state = {}
+
+    render() {
+        return (
+            <>
+                <h1>Carriers JSX</h1>
+            </>
+        )
+    }
+}
