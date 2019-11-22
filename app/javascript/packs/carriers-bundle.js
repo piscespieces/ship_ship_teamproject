b83a3c568c46ec0f5ee4b73389650bd402d63380
Andres Urdaneta <aeum3893@gmail.com>
2019-11-21 19:28:06 -0500

    created carriers-bundle js file

---
+++ app/javascript/packs/carriers-bundle.js
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails';
+
+import Carriers from '../bundles/Carriers/components/Carriers';
+
+// This is how react_on_rails can see the Carriers in the browser.
+ReactOnRails.register({
+    Carriers,
+});
