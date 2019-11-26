55cf13e92a3127921c5157b448a89d568529dcc4
Andres Urdaneta <aeum3893@gmail.com>
2019-11-26 17:28:25 -0500

    added LandingPage to the ReactOnRails render function again

--- app/javascript/packs/carriers-bundle.js
+++ app/javascript/packs/carriers-bundle.js
@@ -5,4 +5,5 @@ import Carriers from '../bundles/Carriers/components/Carriers';
 // This is how react_on_rails can see the Carriers in the browser.
 ReactOnRails.register({
     Carriers,
+    LandingPage
 });
