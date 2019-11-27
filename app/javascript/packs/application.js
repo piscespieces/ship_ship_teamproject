7c2b592804fdc1b3518a20027fb0ac95b8a2573d
Andy Weiss <andyweiss1982@gmail.com>
2019-11-27 17:15:24 -0500

    loading state on form submit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,28 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener('turbolinks:load', function(){
+  const  handleDropdown = () => {
+    document.getElementById("devise-myDropdown").classList.toggle("show")
+  }
+  window.onclick = (event) =>{
+    if (!event.target.matches('.devise-dropbtn')) {
+      const dropdowns = document.getElementsByClassName("devise-dropdown-content")
+      for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i]
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show')
+        }
+      }
+    }
+  }
+
+  const drop = document.querySelector('#devise-drop')
+  if (drop) drop.addEventListener('click', handleDropdown)
+})
+
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
