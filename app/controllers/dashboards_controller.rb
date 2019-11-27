2b0e8d8f32f6abfe0b4ed2173b5a6dd4e1c091c5
Andy Weiss <andyweiss1982@gmail.com>
2019-11-27 16:51:16 -0500

    use data content to populate hover info

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController 
   def show
-    @locations = current_user.locations
+    @locations = current_user&.locations || []
     @keys = [:street1, :street2, :city, :state, :zip, :country]
   end
 end 
