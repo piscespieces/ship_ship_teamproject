df3d5e58aee7e4d19cc3dfe9d8411d63637df74e
Andres Urdaneta <aeum3893@gmail.com>
2019-11-28 16:05:38 -0500

    tweaked the react typewritter

--- app/javascript/bundles/LandingPage/components/TypeWritter.jsx
+++ app/javascript/bundles/LandingPage/components/TypeWritter.jsx
@@ -7,7 +7,7 @@ const TypeWritter = () => {
          <Typist.Delay ms={200} />
          Let us find you < br /> the BEST < br /> shipping options...<br />
          <Typist.Delay ms={500} />for any
-               < Typist.Delay ms={500} /> and ALL NEEDS.< Typist.Backspace count={1} delay={400} /><Typist.Delay ms={500} />!<br />
+               < Typist.Delay ms={500} /> and ALL NEEDS.< Typist.Backspace count={1} delay={400} /><Typist.Delay ms={500} />!!!<br />
       </Typist >
    );
 }
