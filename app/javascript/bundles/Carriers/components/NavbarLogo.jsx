949c66f1614e5bd07954656d0f7f77ce8273aa04
Andres Urdaneta <aeum3893@gmail.com>
2019-12-01 18:26:03 -0500

    anchor tag to the icon in the header

--- app/javascript/bundles/Carriers/components/NavbarLogo.jsx
+++ app/javascript/bundles/Carriers/components/NavbarLogo.jsx
@@ -3,18 +3,20 @@ import React from 'react'
 const NavbarLogo = () => {
    return (
       <>
-         <svg id="navbarlogobutton" viewBox="0 0 441 448" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g id="SHIP SHIP flat Logo 1">
-               <path id="frame-logo" d="M220.127 47H46V401.356H220.127H394.213V47H220.127Z" stroke="black" strokeWidth="12" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-               <g id="leftside-logo">
-                  <path id="Vector" d="M220.545 138.729C243.81 138.729 262.667 157.909 262.667 181.573C262.667 205.237 243.81 224.417 220.545 224.417C197.28 224.417 178.404 243.617 178.404 267.281C178.404 290.945 197.26 310.125 220.525 310.125C243.79 310.125 262.647 329.305 262.647 352.969C262.647 376.633 243.79 395.813 220.525 395.813H52V53H220.545C197.28 53 178.404 72.1799 178.404 95.844C178.404 119.508 197.26 138.688 220.525 138.688V138.729H220.545Z" fill="#E0E5FA" />
-                  <path id="Vector_2" d="M220.545 53C197.281 53 178.404 72.1799 178.404 95.844C178.404 119.508 197.26 138.688 220.525 138.688" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-                  <path id="Vector_3" d="M220.545 224.417C243.81 224.417 262.667 205.237 262.667 181.573C262.667 157.909 243.81 138.729 220.545 138.729" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-                  <path id="Vector_4" d="M220.545 224.417C197.281 224.417 178.424 243.597 178.424 267.261C178.424 290.925 197.281 310.105 220.545 310.105" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-                  <path id="Vector_5" d="M220.545 395.834C243.81 395.834 262.667 376.654 262.667 352.99C262.667 329.326 243.81 310.146 220.545 310.146" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+         <a href="/">
+            <svg id="navbarlogobutton" viewBox="0 0 441 448" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <g id="SHIP SHIP flat Logo 1">
+                  <path id="frame-logo" d="M220.127 47H46V401.356H220.127H394.213V47H220.127Z" stroke="black" strokeWidth="12" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                  <g id="leftside-logo">
+                     <path id="Vector" d="M220.545 138.729C243.81 138.729 262.667 157.909 262.667 181.573C262.667 205.237 243.81 224.417 220.545 224.417C197.28 224.417 178.404 243.617 178.404 267.281C178.404 290.945 197.26 310.125 220.525 310.125C243.79 310.125 262.647 329.305 262.647 352.969C262.647 376.633 243.79 395.813 220.525 395.813H52V53H220.545C197.28 53 178.404 72.1799 178.404 95.844C178.404 119.508 197.26 138.688 220.525 138.688V138.729H220.545Z" fill="#E0E5FA" />
+                     <path id="Vector_2" d="M220.545 53C197.281 53 178.404 72.1799 178.404 95.844C178.404 119.508 197.26 138.688 220.525 138.688" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                     <path id="Vector_3" d="M220.545 224.417C243.81 224.417 262.667 205.237 262.667 181.573C262.667 157.909 243.81 138.729 220.545 138.729" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                     <path id="Vector_4" d="M220.545 224.417C197.281 224.417 178.424 243.597 178.424 267.261C178.424 290.925 197.281 310.105 220.545 310.105" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                     <path id="Vector_5" d="M220.545 395.834C243.81 395.834 262.667 376.654 262.667 352.99C262.667 329.326 243.81 310.146 220.545 310.146" stroke="black" strokeWidth="10" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                  </g>
                </g>
-            </g>
-         </svg>
+            </svg>
+         </a>
       </>
    )
 }
